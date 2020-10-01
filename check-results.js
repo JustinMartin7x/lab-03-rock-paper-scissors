4d458c14e4a343c32384afbbddff5c7f6bfde2e2
dl-watson <dlarkinwatson@gmail.com>
2020-10-01 15:00:47 -0700

    refactoring from partner programming lab exercise

--- check-results.js
+++ check-results.js
@@ -1,9 +1,25 @@
+
+export function randomThrow() {
+    const thrown = Math.ceil(Math.random() * 3);
+
+    if (thrown === 1) {
+        return 'rock';
+    }
+    if (thrown === 2) {
+        return 'paper';
+    }
+    if (thrown === 3) {
+        return 'scissors';
+    }
+}
+
 //compare random throws to user input
 export function doesUserWin(userGuess, randomThrow) {
+
     if (userGuess === randomThrow) {
         return 'draw';
     }
-    if (userGuess === 'scissors' && randomThrow === 'paper'){
+    if (userGuess === 'scissors' && randomThrow === 'paper') {
         return 'win';
     }
     if (userGuess === 'scissors' && randomThrow === 'rock') {
