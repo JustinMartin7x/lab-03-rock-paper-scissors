322053cddc044571faf6d5fa428cb096f4ea4511
JustinMartin7x <justin.martin7x@gmail.com>
2020-09-30 20:41:35 -0700

    styled my page added reset button and finished JSS work on funtions

--- app.js
+++ app.js
@@ -3,56 +3,33 @@ const results = document.getElementById('results');
 const readyButton = document.getElementById('ready');
 const totalSpan = document.getElementById('total');
 const percentSpan = document.getElementById('percent');
+const winsSpan = document.getElementById('wins');
 // const userGuessButton = document.querySelector(':checked');
-
 import { randomThrow } from './utils.js';
 import { doesUserWin } from './check-results.js';
-
 // console.log(readyButton, totalSpan, percentSpan, userGuess);
-
-
 // initialize state
-
-let wins = 0;
+let totalWins = 0;
 let gamesPlayed = 0;
 let gamesTied = 0;
-
-// set event listeners to update state and DOM
-
 readyButton.addEventListener('click', () => {
-
-
     gamesPlayed++;
-
-    const userGuess = document.querySelector('input[name="choice"]:checked');
+    const userGuess = document.querySelector('input[name="choice"]:checked').value;
     const random = randomThrow();
-// console.log(random);
-
-    
-    
-
-    doesUserWin(userGuess.value, random); 
-
-    if (doesUserWin === 'win') {
+    // console.log(userGuess, random);
+    const answer = doesUserWin(userGuess, random); 
+    if (answer === 'win') {
         results.textContent = 'Congratulations!! You Win';
-        wins++;
-    } if (doesUserWin === 'lose') {
+        totalWins++;
+    } if (answer === 'lose') {
         results.textContent = 'Oh No You Lose! try again!!';
-    } if (doesUserWin === 'draw') {
+    } if (answer === 'draw') {
         results.textContent = 'Arggg!! you tied!!';
         gamesTied++;
     }
-    
-
-
-    
-    
-
-// sees which one wins
-// changes states
-
-
-
+    const percent = ((totalWins / gamesPlayed) * 100);
+    winsSpan.textContent = `Wins! ${totalWins}`;
+    totalSpan.textContent = `Total Games Played ${gamesPlayed}`;
+    percentSpan.textContent = `${percent} % of Games Won!`;
 });
-
 
